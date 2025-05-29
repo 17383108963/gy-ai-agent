5c9c4850bda15908dbd164325a294c6fc868ba0e
17383108963 <2493239529@qq.com>
2025-05-29 23:12:02 +0800

    部分修改

---
+++ src/main/java/com/gy/gyaiagent/advisor/MyCustomAdvidor.java
@@ -0,0 +1,52 @@
+package com.gy.gyaiagent.advisor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.advisor.api.*;
+import org.springframework.ai.chat.model.MessageAggregator;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+public class MyCustomAdvidor implements CallAroundAdvisor, StreamAroundAdvisor {
+
+
+	@Override
+	public String getName() { 
+		return this.getClass().getSimpleName();
+	}
+
+	@Override
+	public int getOrder() { 
+		return 0;
+	}
+
+	@Override
+	public AdvisedResponse aroundCall(AdvisedRequest advisedRequest, CallAroundAdvisorChain chain) {
+
+		advisedRequest = this.before(advisedRequest);
+
+		AdvisedResponse advisedResponse = chain.nextAroundCall(advisedRequest);
+
+		this.observeAfter(advisedResponse);
+
+		return advisedResponse;
+	}
+
+	private void observeAfter(AdvisedResponse advisedResponse) {
+		log.info("AI Response: {}", advisedResponse.response().getResult().getOutput().getText());
+	}
+
+	private AdvisedRequest before(AdvisedRequest advisedRequest) {
+		log.info("AI Request: {}", advisedRequest.userText());
+		return advisedRequest;
+	}
+
+	@Override
+	public Flux<AdvisedResponse> aroundStream(AdvisedRequest advisedRequest, StreamAroundAdvisorChain chain) {
+
+		advisedRequest = this.before(advisedRequest);
+
+		Flux<AdvisedResponse> advisedResponses = chain.nextAroundStream(advisedRequest);
+
+        return new MessageAggregator().aggregateAdvisedResponse(advisedResponses, this::observeAfter); 
+	}
+}
--- src/main/java/com/gy/gyaiagent/app/LoveApp.java
+++ src/main/java/com/gy/gyaiagent/app/LoveApp.java
@@ -1,5 +1,8 @@
 package com.gy.gyaiagent.app;
 
+import com.gy.gyaiagent.advisor.MyCustomAdvidor;
+import com.gy.gyaiagent.advisor.ReReadingAdvisor;
+import com.gy.gyaiagent.chatmemory.FileBasedChatMemory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -8,6 +11,8 @@ import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
@@ -28,12 +33,19 @@ public class LoveApp {
             "引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。";
 
     public LoveApp(ChatModel dashscopeChatModel) {
-        // 初始化基于内存的对话记忆
-        ChatMemory chatMemory = new InMemoryChatMemory();
+        // 初始化基于文件的对话记忆
+        String fileDir = System.getProperty("user.dir") + "/tmp/chat-memory";
+        FileBasedChatMemory chatMemory = new FileBasedChatMemory(fileDir);
+//        // 初始化基于内存的对话记忆
+//        ChatMemory chatMemory = new InMemoryChatMemory();
         chatClient = ChatClient.builder(dashscopeChatModel)
                 .defaultSystem(SYSTEM_PROMPT)
                 .defaultAdvisors(
-                        new MessageChatMemoryAdvisor(chatMemory)
+                        new MessageChatMemoryAdvisor(chatMemory),
+                        //自定义advisor,按需开启
+                        new MyCustomAdvidor()
+                        //自定义推理增强Read2advisor，按需开启
+                        //,new ReReadingAdvisor()
                 )
                 .build();
     }
@@ -49,6 +61,21 @@ public class LoveApp {
         String content = response.getResult().getOutput().getText();
         log.info("content: {}", content);
         return content;
+    }
+
+    record LoveReport(String title, List<String> suggestions){}
+
+    public LoveReport doChatWithReport(String message, String chatId) {
+        LoveReport loveReport = chatClient
+                .prompt()
+                .system(SYSTEM_PROMPT + "每次对话后都要生成恋爱结果，标题为{用户名}的恋爱报告，内容为建议列表")
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .call()
+                .entity(LoveReport.class);
+        log.info("loveReport: {}", loveReport);
+        return loveReport;
     }
 
 }
